426633b14c153a4c304da2b4fc73d41aa13f1777
Vlad <hackerjavaVlad@gmail.com>
2018-01-03 20:16:31 +0300

    Add files via upload

---
+++ lesson6/hosting/js/custom.js
@@ -0,0 +1,81 @@
+$(function() {
+
+    //code
+
+
+    // scroll bottom click
+   	$(".js_click_btn").on("click", function(e){
+
+   		e.preventDefault();
+
+   		var plansOffset = $("#js-plans").offset().top;
+
+   			$("html, body").animate({
+   				scrollTop: plansOffset
+   			}, 1000);
+
+   	});
+
+
+
+       // scroll top click
+   	$("#js-scroll-top").on("click", function(e){
+
+   		e.preventDefault();
+
+   		var topNav = 0;
+
+   			$("html, body").animate({scrollTop: topNav}, 1000);
+
+   	});
+
+
+
+
+      // fixed nav menu
+
+     var headerH = $("#js-header").height() , 
+         navH = $("#js-nav-container").innerHeight();
+
+      $(document).on("scroll", function(){
+
+         var docScroll = $(this).scrollTop();
+
+         if (docScroll > headerH) {
+
+            $("#js-nav-container").addClass("fixed");
+
+            $("#js-header").css("paddingTop", navH);
+
+         }
+         else{
+
+            $("#js-nav-container").removeClass("fixed");
+
+            $("#js-header").removeAttr("style");
+
+         }
+
+      });
+
+
+
+
+      // linsk block
+
+      $("#js-nav a").on("click", function(e)
+      {
+
+          e.preventDefault();
+
+          var curentBlock = $(this).attr("href");
+
+              curentBlockOffset = $(curentBlock).offset().top;
+
+             $("html, body").animate({scrollTop: (curentBlockOffset - 20)}, 1000);
+
+      });
+
+      
+
+});
